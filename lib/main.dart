8af8ec2077dacdc1e02f6331f9e3b69a7ff51f57
Made Indra <made.indra@protonmail.com>
2021-02-22 22:06:07 +0700

    create audio button

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  void playSound(String fileName) {
+    player.play(fileName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    playSound('note1.wav');
+                  },
+                  child: Text('One')),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note2.wav');
+                  },
+                  child: Text('Two')),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note3.wav');
+                  },
+                  child: Text('Three')),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note4.wav');
+                  },
+                  child: Text('Four')),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note5.wav');
+                  },
+                  child: Text('Five')),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note6.wav');
+                  },
+                  child: Text('Six')),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note7.wav');
+                  },
+                  child: Text('Seven')),
+            ],
+          ),
         ),
       ),
     );
